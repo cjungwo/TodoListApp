2650466790abbc975a5764736793cbc3f6cc0c1e
Cjungwo <jwoo516@naver.com>
2024-08-21 11:07:57 +1000

    Initial Commit

---
+++ Tutorial3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tutorial3
+//
+//  Created by JungWoo Choi on 21/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
